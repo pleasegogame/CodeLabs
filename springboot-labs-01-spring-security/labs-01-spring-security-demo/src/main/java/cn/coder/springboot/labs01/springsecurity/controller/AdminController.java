730f88e6458ac560769153b0cd4db208107b8424
姚野 <zuiaiyaoye@qq.com>
2023-10-20 10:56:03 +0800

    springsecurity demo

---
+++ springboot-labs-01-spring-security/labs-01-spring-security-demo/src/main/java/cn/coder/springboot/labs01/springsecurity/controller/AdminController.java
@@ -0,0 +1,15 @@
+package cn.coder.springboot.labs01.springsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+    @GetMapping("/demo")
+    public String demo(){
+        return "测试";
+    }
+}
